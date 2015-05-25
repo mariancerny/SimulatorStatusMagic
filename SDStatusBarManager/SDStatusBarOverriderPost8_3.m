50bc1b1690d298f0920058587652d7a5976d7f85
Marián Černý <marian@pematon.com>
2015-05-25 19:59:45 +0200

    Remove battery percentage

--- SDStatusBarManager/SDStatusBarOverriderPost8_3.m
+++ SDStatusBarManager/SDStatusBarOverriderPost8_3.m
@@ -133,7 +133,7 @@ typedef struct {
   overrides->booloverrideItemIsEnabled[8] = 1;
   overrides->values.boolitemIsEnabled[8] = 1;
   overrides->overrideBatteryDetailString = 1;
-  strcpy(overrides->values.batteryDetailString, [@"100%" cStringUsingEncoding:NSUTF8StringEncoding]);
+  strcpy(overrides->values.batteryDetailString, [@"" cStringUsingEncoding:NSUTF8StringEncoding]);
   
   // Bluetooth
   overrides->booloverrideItemIsEnabled[11] = self.bluetoothEnabled;
